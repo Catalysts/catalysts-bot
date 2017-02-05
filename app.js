6d0574b3934728f3396f721d605953152d8d8b2e
Thomas Feichtinger <thomas.feichtinger@catalysts.cc>
2017-02-05 20:15:25 +0100

    remove empty line

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
+
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
@@ -124,7 +125,7 @@ function menu_lack(callback) {
 
 	request(url, function(error, response, html) {
 		if (!error) {
-			
+			//TODO
 		}
 	})
 }
