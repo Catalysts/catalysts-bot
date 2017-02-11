96ed9980281ffbdb5b51da1582be90a0485128a5
Thomas Feichtinger <thomas.feichtinger@catalysts.cc>
2017-02-11 17:38:22 +0100

    move mittagat.js to util, use in menus

--- menus/krauterspiele.js
+++ menus/krauterspiele.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var cheerio = require('cheerio');
-var mittag = require('./mittagat.js')
+var mittag = require('../util/mittagat.js')
 
 module.exports = {
     intent: /.*kraut|kräuterspiele.*/i,
