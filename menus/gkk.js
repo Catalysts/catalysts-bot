4c0af7eeccc6c892bf25f03c1fcc8f770d13eb61
Thomas Feichtinger <Thomas.Feichtinger@catalysts.cc>
2017-12-27 12:45:44 +0100

    check undefined

--- menus/gkk.js
+++ menus/gkk.js
@@ -57,7 +57,11 @@ module.exports = {
                  */
             var index = day * 2;
 
-            var menu = results[index].trim();
+            var menu = results[index];
+            if (!menu) {
+               return result;
+            }
+            menu = menu.trim()
             menu = decodeURIComponent(menu);
             menu = menu.replace(/Classic (I+)/g, "\n\n*Classic $1*\n\n");
             menu = menu.replace("GUSTO", "\n\n*Gusto*\n\n");
