df5c6f62046d1711eaaae3fa948f24a5a8b4c2e0
Thomas Feichtinger <Thomas.Feichtinger@catalysts.cc>
2018-11-14 15:17:37 +0100

    remove unused require

--- menus/lackingerimbiss.js
+++ menus/lackingerimbiss.js
@@ -1,10 +1,6 @@
 "use strict";
 
-var request = require('request');
-var cheerio = require('cheerio');
-var currentWeekNumber = require('current-week-number');
-var PDFParser = require('pdf2json');
-var mittag = require('../util/mittagat.js')
+var mittag = require('../util/mittagat.js');
 
 module.exports = {
     intent: /.*lackingers-imbiss.*/i,
