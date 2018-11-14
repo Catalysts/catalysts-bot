237f8dd03da7dbdeffe73ab1ebaac32135d09013
Thomas Feichtinger <Thomas.Feichtinger@catalysts.cc>
2018-11-14 14:39:24 +0100

    remove unused require

--- menus/hofkneipe.js
+++ menus/hofkneipe.js
@@ -1,9 +1,5 @@
 "use strict";
 
-var request = require('request');
-var cheerio = require('cheerio');
-var currentWeekNumber = require('current-week-number');
-var PDFParser = require('pdf2json');
 var mittag = require('../util/mittagat.js')
 
 module.exports = {
